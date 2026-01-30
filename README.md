af4c327e184039530eb839a39428428dd3438415
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2026-01-30 17:06:58 -0600

    Completed SQL queries

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Weather Data Pipeline
 
-A Python ETL pipeline that collects real-time weather data from the OpenWeatherMap API and stores it in a SQLLite database for analysis.
+A Python ETL pipeline that collects real-time weather data from the OpenWeatherMap API and stores it in a SQLite database for analysis.
 
 ## Features
 - Tracks various weather metrics for 5 major US cities
 - Automated hourly data collection
 - SQLite database
 - Scheduled pipeline using Python
+- SQL queries for analysis
 
 ## Technologies
 - Python 3.x
@@ -24,7 +25,14 @@ cd weather-data-pipeline
 2. Create virtual environment
 ```
 python -m venv venv
+```
+Windows
+```
 source venv\scripts\activate
+```
+Mac/Linux
+```
+source venv/bin/activate
 ```
 
 3. Install dependencies
@@ -41,7 +49,7 @@ Run the pipeline once
 python extract_weather.py
 ```
 
-run the scheduler
+Run the scheduler
 ```
 python scheduler.py
 ```
--- analyze_data.py
+++ analyze_data.py
@@ -6,24 +6,82 @@ def run_query(query):
     df = pd.read_sql_query(query, conn)
     conn.close()
     
+    print("\nQuery results:")
+    print("--------------------")
     print(df)
+
     return df
 
-
+# Quick recent weather check for each city
 query1 = """
--- show weather for each city
 SELECT
-    city, temperature, weather_main
-    FROM weather_data;
+    city, temperature, weather_main, timestamp
+    FROM weather_data
+    WHERE timestamp = (SELECT MAX(timestamp) FROM weather_data)
+    GROUP BY city
+    ORDER BY city;
 """
 
-
+# Rank cities by average temperature descending
 query2 = """
--- show weather for each city
 SELECT
-    *
-    FROM weather_data;
+    city, AVG(temperature) as avg_temperature
+    FROM weather_data
+    GROUP BY city
+    ORDER BY avg_temperature DESC;
 """
+
+# Most humid city on average
+query3 = """
+SELECT
+    city, AVG(humidity) as avg_humidity
+    FROM weather_data
+    GROUP BY city
+    ORDER BY avg_humidity DESC
+    LIMIT 1;
+"""
+
+# Temperature range and stats for each city
+query4 = """
+SELECT
+    city, 
+    MAX(temperature) as max_temp, 
+    MIN(temperature) as min_temp, 
+    MAX(temperature) - MIN(temperature) as temp_range
+    FROM weather_data
+    GROUP BY city;
+"""
+
+# Weather condition frequency
+query5 = """
+SELECT
+    city, weather_main, COUNT(*) as condition_count
+    FROM weather_data
+    GROUP BY city, weather_main;
+"""
+
+# Hourly temperature trends
+query6 = """
+SELECT
+    city, timestamp, temperature,
+    LAG(temperature) OVER (PARTITION BY city ORDER BY timestamp) as previous_temp,
+    temperature - LAG(temperature) OVER (PARTITION BY city ORDER BY timestamp) as temp_change
+    FROM weather_data
+    ORDER BY city, timestamp;
+"""
+
+# running 3 hour average temperature for each city 
+query7 = """
+SELECT
+    city, timestamp, temperature,
+    AVG(temperature) 
+    OVER (PARTITION BY city ORDER BY timestamp ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) 
+    as three_hour_avg_temp
+    FROM weather_data
+    ORDER BY city, timestamp;
+"""
+
 
 if __name__ == "__main__":
-    run_query(query2)
+    run_query(query1)
+
